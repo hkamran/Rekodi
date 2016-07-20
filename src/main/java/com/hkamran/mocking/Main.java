e52d9289f06dfcb898272566d603d96de2aaf093
hkamran <hkamran490@gmail.com>
2016-07-20 15:24:19 -0400

    -added commandline

--- src/main/java/com/hkamran/mocking/Main.java
+++ src/main/java/com/hkamran/mocking/Main.java
@@ -1,5 +1,11 @@
 package com.hkamran.mocking;
 
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -13,40 +19,88 @@ import com.hkamran.mocking.servers.WebSocket;
  */
 public class Main {
 
+	private static final int DEFAULT_PROXY_PORT = 9090;
+	private static final int DEFAULT_WEB_PORT = 8090;
+	
 	private final static Logger log = LogManager.getLogger(Main.class);
 
-	public static void main(String[] args) throws Exception {
-		
-		/**
-		 * Setup Filter
-		 */
-		
-		log.info("Starting up Recorder....");
-		
-//		Filter filter = new Filter();
-
-//		
-//		
-		HTTPServer frontEnd = new HTTPServer();
-//	frontEnd.addFilter("default", filter);
-		
-		Proxies proxies = new Proxies();
-		Integer id = proxies.add("Default Service", 9090);
-		Proxy proxy = proxies.get(id);
-		Filter filter = proxy.getFilter();
-		
-		Integer port = 80;
-		String host = "www.thomas-bayer.com";
-		
-		filter.setState(State.PROXY);
-		filter.setRedirectInfo(host, port);
-		filter.setRedirectState(true);
-		
-		/**
-		 * Start Program
-		 */
-		WebSocket.setProxyManager(proxies);
-		HTTPServer.setProxyManager(proxies);
-		frontEnd.start(8090);
+	
+	public static void main(String[] args) {
+		try {
+			Options options = createCMDOptions();
+			HelpFormatter f = new HelpFormatter();
+			CommandLineParser parser = new BasicParser();
+			CommandLine cmd;
+			f.setWidth(250);
+			
+			
+			cmd = parser.parse(options, args, true);
+			Integer webPort = getWebPort(cmd);
+			Integer initialProxy = getProxyPort(cmd);
+			
+			/**
+			 * Setup Filter ..
+			 * 
+			 */
+			
+			log.info("Starting up Recorder....");
+			Proxies proxies = new Proxies();
+			
+			Integer id = proxies.add("Default Service", initialProxy);
+			Proxy proxy = proxies.get(id);
+			Filter filter = proxy.getFilter();
+			
+			filter.setState(State.PROXY);
+			filter.setRedirectInfo("www.thomas-bayer.com", 80);
+			filter.setRedirectState(true);
+			
+			
+			/**
+			 * Start Program
+			 */
+			WebSocket.setProxyManager(proxies);
+			HTTPServer.setProxyManager(proxies);
+			
+			HTTPServer frontEnd = new HTTPServer();
+			
+			log.info("Startup finished! ");
+			frontEnd.start(webPort);
+	
+		} catch (Exception e) {
+			System.err.println("Unable to start application...");
+			e.printStackTrace();
+		}
 	}
+
+	private static Integer getProxyPort(CommandLine cmd) {
+		Integer webPort = DEFAULT_PROXY_PORT;
+		if (cmd.hasOption("proxyPort")) {
+			webPort = Integer.parseInt(cmd.getOptionValue("proxyPort"));
+		}
+		return webPort;
+	}
+	
+	private static Integer getWebPort(CommandLine cmd) {
+		Integer webPort = DEFAULT_WEB_PORT;
+		if (cmd.hasOption("webPort")) {
+			webPort = Integer.parseInt(cmd.getOptionValue("webPort"));
+		}
+		return webPort;
+	}
+	
+	private static Options createCMDOptions() {
+		Option webPort = new Option("webPort", true,
+				"Port for the web gui");
+		webPort.setArgName("webPort");
+		webPort.setRequired(false);
+		
+		Option proxyPort = new Option("proxyPort", true,
+				"Port for the initial proxy");
+		proxyPort.setArgName("proxyPort");
+		proxyPort.setRequired(false);
+
+		Options options = new Options();
+		options.addOption(webPort);
+		return options;
+}
 }
